2850d51fbe4ca26cff36d7d5863e7119f37340d6
iamded <dedetmasri@gmail.com>
2023-05-30 10:03:20 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+echo "Dont forget subcribe my chanel: iam ded !!!..."
+sleep 3
+termux-open-url https://youtube.com/channel/UCk0IHP3kOrqN86sXjs1c_Xw
+clear
+sleep 3
+echo "Checking package harap menunggu ya bos..!!!"
+sleep 3
+pkg install python bash git -y
+pip install requests colorama bs4
+pip2 install colorama bs4 requests
+pkg update -y
+pkg upgrade -y
+echo "Package alredy install - running tools"
+clear
+sleep 5
+git clone https://github.com/Gameye98/vbug
+cd vbug
+clear
+sleep 3
+python vbug.py
